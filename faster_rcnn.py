e19090a7483a76dfefbd433b6e975eedac82af4f
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-10-11 20:06:10 -0400

    next is create_architecture

--- faster_rcnn.py
+++ faster_rcnn.py
@@ -39,6 +39,10 @@ class faster_rcnn(object):
         # configuration parameter holders
         self._cfg = DEFAULT_CFG
         self._configure()
+        # variable holders
+        self._predictions={}
+        self._anchor_targets={}
+        self._proposal_targets={}
 
     def configure(self,fname):
         import config
@@ -121,10 +125,10 @@ class faster_rcnn(object):
             raise NotImplementedError
 
         #self._predictions["rpn_cls_score"] = rpn_cls_score
-        #self._predictions["rpn_cls_score_reshape"] = rpn_cls_score_reshape
+        self._predictions["rpn_cls_score_reshape"] = rpn_cls_score_reshape
         #self._predictions["rpn_cls_prob"] = rpn_cls_prob
         #self._predictions["rpn_cls_pred"] = rpn_cls_pred
-        #self._predictions["rpn_bbox_pred"] = rpn_bbox_pred
+        self._predictions["rpn_bbox_pred"] = rpn_bbox_pred
         #self._predictions["rois"] = rois
 
         return rois
@@ -133,7 +137,7 @@ class faster_rcnn(object):
         net = tf.reshape(net,(self._cfg.TRAIN.BATCH_SIZE,-1),name='fake_head_to_tail')
         return net
 
-    def _build(self, net, trainable=True):
+    def _build_network(self, net, trainable=True):
         # select initializers
         if self._cfg.TRAIN.TRUNCATED:
             initializer = tf.truncated_normal_initializer(mean=0.0, stddev=0.01)
@@ -164,6 +168,86 @@ class faster_rcnn(object):
 
         return rois, cls_prob, bbox_pred
 
+    def _add_losses(self, sigma_rpn=3.0):
+        with tf.variable_scope('LOSS') as scope:
+            # RPN, class loss
+            # Object present/absent classification for _every_ anchor (# channel =2)
+            rpn_cls_score = tf.reshape(self._predictions['rpn_cls_score_reshape'], [-1, 2])
+            # Object present/absent label for _every_ anchor
+            rpn_label = tf.reshape(self._anchor_targets['rpn_labels'], [-1])
+            rpn_select = tf.where(tf.not_equal(rpn_label, -1))
+            rpn_cls_score = tf.reshape(tf.gather(rpn_cls_score, rpn_select), [-1, 2])
+            rpn_label = tf.reshape(tf.gather(rpn_label, rpn_select), [-1])
+            rpn_cross_entropy = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(logits=rpn_cls_score, labels=rpn_label))
+
+            # RPN, bbox loss
+            # Get rpn_bbox_pred from region_proposal
+            # ... which is a prediction of objecxt location based on the anchor location
+            rpn_bbox_pred = self._predictions['rpn_bbox_pred']
+            # Get rpn_bbox_targets from anchor_target_layer
+            # (which is "true distance" of an anchor to a corresponding truth box location)
+            rpn_bbox_targets = self._anchor_targets['rpn_bbox_targets']
+            rpn_bbox_inside_weights = self._anchor_targets['rpn_bbox_inside_weights']
+            rpn_bbox_outside_weights = self._anchor_targets['rpn_bbox_outside_weights']
+            rpn_loss_box = self._smooth_l1_loss(rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights,
+                                                rpn_bbox_outside_weights, sigma=sigma_rpn, dim=[1, 2, 3])
+            
+            # RCNN, class loss
+            # Get cls_score from region_classification, dim = (#box, #class)
+            cls_score = self._predictions["cls_score"]
+            # Get label from  proposal_target_layer, dim = (#box,1) 
+            # ... the value in the axis=1 of "label" is class type integer (0=>22 in case of faster rcnn)
+            label = tf.reshape(self._proposal_targets["labels"], [-1])
+            # Because label is just an integer (i.e. not a "hot label" array), use sparse
+            cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=cls_score, labels=label))
+
+            # RCNN, bbox loss
+            # Get bbox_pred from region_classification, dim = (#box, 4)
+            # (which is a prediction from cropped ROI region to a better truth box location)
+            bbox_pred = self._predictions['bbox_pred']
+            # Get bbox_targets from region_classification 
+            # (which is "true distance" of a cropped ROI region to a corresponding truth box location)
+            bbox_targets = self._proposal_targets['bbox_targets']
+            # ???
+            bbox_inside_weights = self._proposal_targets['bbox_inside_weights']
+            # ???
+            bbox_outside_weights = self._proposal_targets['bbox_outside_weights']
+            # Now make L1 loss
+            loss_box = self._smooth_l1_loss(bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights)
+            
+            #self._losses['cross_entropy'] = cross_entropy
+            #self._losses['loss_box'] = loss_box
+            #self._losses['rpn_cross_entropy'] = rpn_cross_entropy
+            #self._losses['rpn_loss_box'] = rpn_loss_box
+            
+            loss = cross_entropy + loss_box + rpn_cross_entropy + rpn_loss_box
+
+            if len(tf.losses.get_regularization_losses())>0:
+                regularization_loss = tf.add_n(tf.losses.get_regularization_losses(), 'regu')
+                self._losses['total_loss'] = loss + regularization_loss
+            else:
+                print('\033[95mWARNING\033[00m no weights regularizer found ... you sure????')
+                
+            #self._event_summaries.update(self._losses)
+            
+            return loss
+
+    def _smooth_l1_loss(self, bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights, sigma=1.0, dim=[1]):
+        sigma_2 = sigma ** 2
+        box_diff = bbox_pred - bbox_targets
+        in_box_diff = bbox_inside_weights * box_diff
+        abs_in_box_diff = tf.abs(in_box_diff)
+        smoothL1_sign = tf.stop_gradient(tf.to_float(tf.less(abs_in_box_diff, 1. / sigma_2)))
+        in_loss_box = tf.pow(in_box_diff, 2) * (sigma_2 / 2.) * smoothL1_sign \
+                      + (abs_in_box_diff - (0.5 / sigma_2)) * (1. - smoothL1_sign)
+        out_loss_box = bbox_outside_weights * in_loss_box
+        loss_box = tf.reduce_mean(tf.reduce_sum(
+            out_loss_box,
+            axis=dim
+        ))
+        return loss_box
+
     #
     # tf.py_func wrappers
     #
@@ -181,8 +265,6 @@ class faster_rcnn(object):
             anchors.set_shape([None,4])
             num_base_anchors.set_shape([])
             self._anchors = anchors
-
-
 
     def _proposal_layer_2d(self,rpn_cls_prob, rpn_bbox_pred, trainable, name):
         with tf.variable_scope(name) as scope:
@@ -215,10 +297,10 @@ class faster_rcnn(object):
         rpn_bbox_outside_weights.set_shape([1, None, None, self._num_base_anchors * 4])
 
         rpn_labels = tf.to_int32(rpn_labels, name="to_int32")
-        #self._anchor_targets['rpn_labels'] = rpn_labels
-        #self._anchor_targets['rpn_bbox_targets'] = rpn_bbox_targets
-        #self._anchor_targets['rpn_bbox_inside_weights'] = rpn_bbox_inside_weights
-        #self._anchor_targets['rpn_bbox_outside_weights'] = rpn_bbox_outside_weights
+        self._anchor_targets['rpn_labels'] = rpn_labels
+        self._anchor_targets['rpn_bbox_targets'] = rpn_bbox_targets
+        self._anchor_targets['rpn_bbox_inside_weights'] = rpn_bbox_inside_weights
+        self._anchor_targets['rpn_bbox_outside_weights'] = rpn_bbox_outside_weights
 
         #self._score_summaries.update(self._anchor_targets)
 
@@ -239,11 +321,11 @@ class faster_rcnn(object):
             bbox_inside_weights.set_shape([self._cfg.TRAIN.BATCH_SIZE, self._num_classes * 4])
             bbox_outside_weights.set_shape([self._cfg.TRAIN.BATCH_SIZE, self._num_classes * 4])
 
-            #self._proposal_targets['rois'] = rois
-            #self._proposal_targets['labels'] = tf.to_int32(labels, name="to_int32")
-            #self._proposal_targets['bbox_targets'] = bbox_targets
-            #self._proposal_targets['bbox_inside_weights'] = bbox_inside_weights
-            #self._proposal_targets['bbox_outside_weights'] = bbox_outside_weights
+            self._proposal_targets['rois'] = rois
+            self._proposal_targets['labels'] = tf.to_int32(labels, name="to_int32")
+            self._proposal_targets['bbox_targets'] = bbox_targets
+            self._proposal_targets['bbox_inside_weights'] = bbox_inside_weights
+            self._proposal_targets['bbox_outside_weights'] = bbox_outside_weights
             
             #self._score_summaries.update(self._proposal_targets)
 
@@ -282,10 +364,10 @@ class faster_rcnn(object):
                                          trainable=trainable,
                                          activation_fn=None, scope='bbox_pred')
 
-        #self._predictions["cls_score"] = cls_score
-        #self._predictions["cls_pred"] = cls_pred
-        #self._predictions["cls_prob"] = cls_prob
-        #self._predictions["bbox_pred"] = bbox_pred
+        self._predictions["cls_score"] = cls_score
+        self._predictions["cls_pred"] = cls_pred
+        self._predictions["cls_prob"] = cls_prob
+        self._predictions["bbox_pred"] = bbox_pred
 
         return cls_prob, bbox_pred
 
@@ -328,11 +410,12 @@ if __name__ == '__main__':
             #net._generate_anchors_2d()
             # create a 0-filled feature map for rpn
             bottom = tf.placeholder(tf.float32,[1,16,32,64])
-            roi,cls_prob, bbox_pred = net._build(net=bottom, trainable=True)
+            roi,cls_prob, bbox_pred = net._build_network(net=bottom, trainable=True)
+            loss = net._add_losses()
             # Create a session
             sess = tf.InteractiveSession()
             # Initialize variables
             sess.run(tf.global_variables_initializer())
-            sess.run([roi,cls_prob,bbox_pred], 
+            sess.run([roi,cls_prob,bbox_pred,loss], 
                      feed_dict = { bottom:np.zeros((1,16,32,64),np.float32),
                                    net._gt_boxes:np.array(((1,10,10,20,20),)) } )
