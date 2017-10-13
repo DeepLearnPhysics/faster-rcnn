75ab81051004ab9942f4cd0e61b4e1ec3a975420
jiwoncpark <jwp@stanford.edu>
2017-10-13 19:22:38 -0400

    jk try again

--- lib/rcnn_train/cocodata.py
+++ lib/rcnn_train/cocodata.py
@@ -58,40 +58,6 @@ class cocodata_gen(object):
 
         return len(self.__class__.CLASSES)
 
-    def get_minibatch(roidb, num_classes):
-        """Given a roidb, construct a minibatch sampled from it."""
-        num_images = len(roidb)
-        # Sample random scales to use for each image in this batch
-        random_scale_inds = npr.randint(0, high=len(cfg.TRAIN.SCALES),
-                  size=num_images)
-        assert(cfg.TRAIN.BATCH_SIZE % num_images == 0), \
-        'num_images ({}) must divide BATCH_SIZE ({})'. \
-        format(num_images, cfg.TRAIN.BATCH_SIZE)
-
-        # Get the input image blob, formatted for caffe
-        im_blob, im_scales = _get_image_blob(roidb, random_scale_inds)
-
-        blobs = {'data': im_blob}
-        
-        assert len(im_scales) == 1, "Single batch only"
-        assert len(roidb) == 1, "Single batch only"
-  
-        # gt boxes: (x1, y1, x2, y2, cls)
-        if cfg.TRAIN.USE_ALL_GT:
-        # Include all ground truth boxes
-            gt_inds = np.where(roidb[0]['gt_classes'] != 0)[0]
-        else:
-        # For the COCO ground truth boxes, exclude the ones that are ''iscrowd'' 
-            gt_inds = np.where(roidb[0]['gt_classes'] != 0 & np.all(roidb[0]['gt_overlaps'].toarray() > -1.0, axis=1))[0]
-        gt_boxes = np.empty((len(gt_inds), 5), dtype=np.float32)
-        gt_boxes[:, 0:4] = roidb[0]['boxes'][gt_inds, :] * im_scales[0]
-        gt_boxes[:, 4] = roidb[0]['gt_classes'][gt_inds]
-        blobs['gt_boxes'] = gt_boxes
-        blobs['im_info'] = np.array([im_blob.shape[1], im_blob.shape[2], im_scales[0]],
-                                    dtype=np.float32)
-        
-        return blobs
-
     def _get_image_blob(roidb, scale_inds):
         """Builds an input blob from the images in the roidb at the specified
         scales.
@@ -113,19 +79,6 @@ class cocodata_gen(object):
         blob = im_list_to_blob(processed_ims)
 
         return blob, im_scales
-
-    def im_list_to_blob(ims):
-        """Convert a list of images into a network input.
-        Assumes images are already prepared (means subtracted, BGR order, ...). """
-        max_shape = np.array([im.shape for im in ims]).max(axis=0)
-        num_images = len(ims)
-        blob = np.zeros((num_images, max_shape[0], max_shape[1], 3),
-                  dtype=np.float32)
-        for i in range(num_images):
-            im = ims[i]
-            blob[i, 0:im.shape[0], 0:im.shape[1], :] = im
-        return blob
-
 
     def prep_im_for_blob(self, im, pixel_means, target_size, max_size):
         """Mean subtract and scale an image for use in a blob."""
