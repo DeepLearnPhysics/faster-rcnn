a50770476823d7ebc548a694d1721e270028f5bd
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-10-18 22:38:01 -0400

    debugging proposal_layer

--- lib/rcnn_layers/proposal_layer.py
+++ lib/rcnn_layers/proposal_layer.py
@@ -33,14 +33,16 @@ def proposal_layer_2d(rpn_cls_prob, rpn_bbox_pred,
 
   # Get the scores and bounding boxes by keeping only "object exist" entries
   scores = rpn_cls_prob[:, :, :, num_base_anchors:]
+  print('\033[93ma1\033[00m {:s}'.format(scores.reshape([scores.size]).mean()))
   # Reshape to a list in the order of anchors
   rpn_bbox_pred = rpn_bbox_pred.reshape((-1, 4))
   scores = scores.reshape((-1, 1))
   # Transform proposals using rpn_bbox_pred (prediction parameters based on anchors)
   proposals = bbox_transform_inv(anchors, rpn_bbox_pred)
+  print('\033[93ma2\033[00m {:s}'.format(proposals.reshape([proposals.size]).mean()))
   # Clip boxes that are out-of-image loc
   proposals = clip_boxes(proposals, input_shape[1:3])
-
+  print('\033[93ma3\033[00m {:s}'.format(proposals.reshape([proposals.size]).mean()))
   # Create an index-array sorted by the score values
   order = scores.ravel().argsort()[::-1]
 
@@ -49,7 +51,7 @@ def proposal_layer_2d(rpn_cls_prob, rpn_bbox_pred,
     order = order[:pre_nms_topN]
   proposals = proposals[order, :]
   scores = scores[order]
-
+  print('\033[93ma4\033[00m {:s}'.format(scores.reshape([scores.size]).mean()))
   # Non-maximal suppression w/ IoUs threshold across proposed regions
   keep = nms(np.hstack((proposals, scores)), float(nms_thresh))
   #keep_max = len(proposals)
@@ -62,7 +64,7 @@ def proposal_layer_2d(rpn_cls_prob, rpn_bbox_pred,
     keep = keep[:post_nms_topN]
   proposals = proposals[keep, :]
   scores = scores[keep]
-
+  print('\033[93ma5\033[00m {:s}'.format(scores.reshape([scores.size]).mean()))
   # Expand the array via np.hstack and zero-filled array to later store an object class prediction.
   # here we assume only-1-image-batch-size to initialize the array size
   batch_inds = np.zeros((proposals.shape[0], 1), dtype=np.float32)
